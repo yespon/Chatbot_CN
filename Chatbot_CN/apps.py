43353d488c8c93b47603339d82584b1f298681d7
Q_S_Y_Q <charlesxu86@163.com>
2020-01-20 11:39:35 +0800

    readme

--- Chatbot_CN/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RegisterLoginConfig(AppConfig):
-    name = 'Chatbot_Web'
--- start_server.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-nohup python manage.py runserver 172.18.103.43:8008 > /home/souche/logs/chatbot_log 2>&1 &
